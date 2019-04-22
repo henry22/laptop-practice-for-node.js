f64365d82bb9a1ce276a2b9da1328bc46e690dd9
Henry <henry@Henryde-iMac.local>
2019-04-22 14:30:36 +0800

    parse the json data

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const fs = require('fs');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 
-console.log(__dirname);
-console.log(json);
+const laptopData = JSON.parse(json);
