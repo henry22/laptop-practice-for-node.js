00d95df82cd5c5843541668df6c6d54af0f5489f
Henry <henry@Henryde-iMac.local>
2019-04-22 15:15:40 +0800

    parse the url query using url to get more information

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
+    const query = url.parse(req.url);
+    console.log(query);
+
     res.writeHead(200, { 'Content-type': 'text/html' });
     res.end('This is the response');
 });
