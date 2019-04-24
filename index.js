a180bad1607d60aa9f45d7a3a70b9dfc8056d9ea
Henry <henry@Henryde-iMac.local>
2019-04-24 16:59:36 +0800

    create a folder called templates

--- index.js
+++ index.js
@@ -6,11 +6,20 @@ const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-    const query = url.parse(req.url, true);
-    console.log(query);
+    const pathName = url.parse(req.url, true).pathname;
+    const id = url.parse(req.url, true).query.id;
 
-    res.writeHead(200, { 'Content-type': 'text/html' });
-    res.end('This is the response');
+    if(pathName === '/products' || pathName === '/') {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end('This is the PRODUCTS page');
+    } else if(pathName === '/laptop' && id < laptopData.length) {
+        res.writeHead(200, { 'Content-type': 'text/html' });
+        res.end(`This is the LAPTOP page for laptop ${id}`);
+    } else {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        res.end('URL was not found on the server!');
+    }
+
 });
 
 server.listen(1337, '127.0.0.1', () => {
