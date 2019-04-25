945d11d1471c51e65f4881e17cd489901963f389
Henry <henry@Henryde-iMac.local>
2019-04-25 13:50:31 +0800

    write a function to replace the unique symbol with laptop data value

--- index.js
+++ index.js
@@ -9,26 +9,21 @@ const server = http.createServer((req, res) => {
     const pathName = url.parse(req.url, true).pathname;
     const id = url.parse(req.url, true).query.id;
 
+    // PRODUCTAS OVERVIEW
     if(pathName === '/products' || pathName === '/') {
         res.writeHead(200, { 'Content-type': 'text/html' });
         res.end('This is the PRODUCTS page');
+    // LAPTOP DETAIL
     } else if(pathName === '/laptop' && id < laptopData.length) {
         res.writeHead(200, { 'Content-type': 'text/html' });
 
         fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
             let laptop = laptopData[id];
-            let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-
-            output = output.replace(/{%IMAGE%}/g, laptop.image);
-            output = output.replace(/{%CPU%}/g, laptop.cpu);
-            output = output.replace(/{%RAM%}/g, laptop.ram);
-            output = output.replace(/{%STORAGE%}/g, laptop.storage);
-            output = output.replace(/{%SCREEN%}/g, laptop.screen);
-            output = output.replace(/{%PRICE%}/g, laptop.price);
-            output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+            const output = replaceTemplate(data, laptop);
 
             res.end(output);
         });
+    // URL NOT FOUND
     } else {
         res.writeHead(404, { 'Content-type': 'text/html' });
         res.end('URL was not found on the server!');
@@ -39,3 +34,18 @@ const server = http.createServer((req, res) => {
 server.listen(1337, '127.0.0.1', () => {
     console.log('Listening for requests now');
 });
+
+function replaceTemplate(originalHTML, laptop) {
+    let output = originalHTML.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+
+    output = output.replace(/{%IMAGE%}/g, laptop.image);
+    output = output.replace(/{%CPU%}/g, laptop.cpu);
+    output = output.replace(/{%RAM%}/g, laptop.ram);
+    output = output.replace(/{%STORAGE%}/g, laptop.storage);
+    output = output.replace(/{%SCREEN%}/g, laptop.screen);
+    output = output.replace(/{%PRICE%}/g, laptop.price);
+    output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+    output = output.replace(/{%ID%}/g, laptop.id);
+
+    return output;
+}
